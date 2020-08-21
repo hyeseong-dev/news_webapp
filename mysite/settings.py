2ac0c35fd578d94a2d4ad89de5b727475964d43d
osori <hyeseong43@gmail.com>
2020-08-21 15:50:37 +0900

    댓글기능 구현 완료

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,6 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TAGGIT_CASE_INSENSITIVE = True
 TAGGIT_LIMIT = 50
 
-DISQUS_SHORTNAME = 'new_web_app'
+DISQUS_SHORTNAME = 'estagram'
 DISQUS_MY_DOMAIN = 'http://127.0.0.1:8000'
 
